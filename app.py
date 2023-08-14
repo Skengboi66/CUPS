001235ae84d9790cbe7c918f653a4cb7388b646a
Emir Afiq <emirafiq523@gmail.com>
2023-08-14 23:41:51 +0800

    added database changes

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, Primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+
 
 #page routing 
 @app.route('/')
