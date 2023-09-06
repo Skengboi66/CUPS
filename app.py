19fe0e3dd6b5401241c76b013d279c6c0a5fb0cf
Emir Afiq <emirafiq523@gmail.com>
2023-09-06 18:10:19 +0800

    added admin home page and edited login page

--- app.py
+++ app.py
@@ -17,6 +17,17 @@ def home():
 def login():
     return render_template('login.html')
 
+@app.route('/login')
+def login():
+    return render_template('studentHome.html')
+
+@app.route('/login')
+def login():
+    return render_template('supervisorHome.html')
+
+@app.route('/login')
+def login():
+    return render_template('adminHome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
