44ca517dcfa6985f09209d220cf3605e010bf0e0
Emir Afiq <emirafiq523@gmail.com>
2023-09-06 18:39:51 +0800

    jj

--- app.py
+++ app.py
@@ -5,12 +5,14 @@ from flask import Flask, render_template, url_for
 app = Flask (__name__)
 
 
-
-
 #page routing 
 @app.route('/')
 def home():
     return render_template('home.html') 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 
 
