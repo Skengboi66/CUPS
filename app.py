298e728a279cb864ff5c5d6362251806e9cf82d7
Emir Afiq <emirafiq523@gmail.com>
2023-09-06 18:35:17 +0800

    test

--- app.py
+++ app.py
@@ -13,21 +13,7 @@ app = Flask (__name__)
 def home():
     return render_template('home.html') 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
 
-@app.route('/login')
-def login():
-    return render_template('studentHome.html')
-
-@app.route('/login')
-def login():
-    return render_template('supervisorHome.html')
-
-@app.route('/login')
-def login():
-    return render_template('adminHome.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
