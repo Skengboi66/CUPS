d2aea7cdee5fb48f1b3b7ad971afb6d0acb34063
Emir Afiq <emirafiq523@gmail.com>
2023-09-06 17:09:14 +0800

    esited database

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+
 from flask_login import UserMixin
 
 
 app = Flask (__name__)
 
-#database connectivity
-db = SQLAlchemy(app)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite://database.db'
 
-#secret key for session
-app.config['SECRET_KEY']= 'thisisasecretkey'
-
-#Database table
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, Primary_key=True)
-    username = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(80), nullable=False)
-    email = db.Column(db.String(80), nullable=False)
 
 
 #page routing 
