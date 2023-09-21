bf2b53504750c93ace44efce5e29a7ca51f4ad66
Emir Afiq <emirafiq523@gmail.com>
2023-09-21 12:54:46 +0800

    edit

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ class CompanySupervisor(db.Model):
     
 class Feedback(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    student_id = db.Column(db.Integer, db.ForeignKey('users.student_id'), nullable=False)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.student_id'), nullable=False)
     quality_of_work = db.Column(db.Integer)
     communication_skills = db.Column(db.Integer)
     punctuality = db.Column(db.Integer)
@@ -40,7 +40,8 @@ class Feedback(db.Model):
 
 
 
-with app.app_context():
+@app.before_first_request
+def create_tables():
     db.create_all()
 
 
@@ -53,7 +54,7 @@ def home():
 def login():
     return render_template('login.html')
 
-@app.route('feedback')
+@app.route('/feedback')
 def feedback():
     return render_template('studentFeedback.html') 
 
@@ -79,23 +80,23 @@ def admin():
 
 @app.route('/adminLogin', methods=['GET', 'POST'])
 def adminLogin():
+    error_message = None  # Initialize error_message
     if request.method == 'POST':
-            username = request.form['username']
-            password = request.form['password']
+        username = request.form['username']
+        password = request.form['password']
 
-            # Check if the provided username and password match the hardcoded admin credentials
-            if username == 'admin' and password == 'admin123':
-                # Redirect to a dashboard or another page on successful login
-                return redirect(url_for('admin'))
-            else:
-                error_message = "Invalid credentials. Please try again."
+        # Check if the provided username and password match the hardcoded admin credentials
+        if username == 'admin' and password == 'admin123':
+            # Redirect to a dashboard or another page on successful login
+            return redirect(url_for('admin'))
+        else:
+            error_message = "Invalid credentials. Please try again."
 
     return render_template('adminLogin.html', error_message=error_message if 'error_message' in locals() else '')
 
 @app.route('/addAccount', methods=['GET', 'POST'])
 def registration():
     if request.method == 'POST':
-        id = request.form['id']
         name = request.form['name']
         email = request.form['email']
         company = request.form['company']
@@ -103,7 +104,6 @@ def registration():
         company_supervisor = request.form['company_supervisor']
 
         new_user = User(
-            id=id,
             name=name,
             email=email,
             company=company,
@@ -111,23 +111,18 @@ def registration():
             company_supervisor=company_supervisor
         )
 
-        
-
         db.session.add(new_user)
         db.session.commit()
-
 
         return redirect(url_for('registration'))
 
     return render_template('registration.html')
-
 
 @app.route('/addStudent', methods=['GET', 'POST'])
 def add_student():
     error_message = None  # Initialize error_message
     if request.method == 'POST':
         try:
-            id = request.form['id']
             name = request.form['name']
             email = request.form['email']
             company = request.form['company']
@@ -135,7 +130,6 @@ def add_student():
             company_supervisor = request.form['company_supervisor']
 
             new_user = User(
-                id=id,
                 name=name,
                 email=email,
                 company=company,
@@ -151,7 +145,7 @@ def add_student():
             error_message = str(e)
 
     return render_template('adminHome.html', error_message=error_message)
- 
+
 @app.route('/deleteAccount/<int:user_id>', methods=['GET', 'POST'])
 def delete_account(user_id):
     user = User.query.get_or_404(user_id)
@@ -163,7 +157,6 @@ def delete_account(user_id):
 
     return render_template('deleteAccount.html', user=user)
 
-
 @app.route('/provide_feedback/<int:user_id>', methods=['GET', 'POST'])
 def provide_feedback(user_id):
     student = User.query.get_or_404(user_id)
@@ -172,42 +165,28 @@ def provide_feedback(user_id):
         feedback_text = request.form['feedback']
 
         # Create a Feedback object and associate it with the student
-        feedback = Feedback(text=feedback_text, user_id=User.id)
-        db.session.add(feedback)
-        db.session.commit()
-
-        # Redirect to the student's profile page or any other appropriate page
-        return redirect(url_for('supervisor', user_id=User.id))
-
-    return render_template('supervisorHome.html', user=User)
-
-@app.route('/users')
-def view_users():
-    users = User.query.all()
-    return render_template('userList.html', users=users.query.all())
-
-def submit():
-    if request.method == 'POST':
-        criteria = [
-            'Quality of Work',
-            'Communication Skills',
-            'Punctuality',
-            'Teamwork',
-            'Problem Solving'
-        ]
-
         feedback = Feedback(
-            quality_of_work=int(request.form.get('Quality of Work')),
-            communication_skills=int(request.form.get('Communication Skills')),
-            punctuality=int(request.form.get('Punctuality')),
-            teamwork=int(request.form.get('Teamwork')),
-            problem_solving=int(request.form.get('Problem Solving'))
+            student_id=user_id,
+            quality_of_work=int(request.form.get('quality_of_work')),
+            communication_skills=int(request.form.get('communication_skills')),
+            punctuality=int(request.form.get('punctuality')),
+            teamwork=int(request.form.get('teamwork')),
+            problem_solving=int(request.form.get('problem_solving'))
         )
 
         db.session.add(feedback)
         db.session.commit()
 
-        return "Feedback submitted successfully!"
+        # Redirect to the student's profile page or any other appropriate page
+        return redirect(url_for('supervisor', user_id=user_id))
+
+    return render_template('supervisorHome.html', student=student)
+
+@app.route('/users')
+def view_users():
+    users = User.query.all()
+    return render_template('userList.html', users=users)
+
     
     
 if __name__ == '__main__':
